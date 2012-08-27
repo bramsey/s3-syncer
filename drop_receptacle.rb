933cd69d30a76a82e5645b5636c3d99f27d24e61
Bill Ramsey <bill@billiamram.com>
2012-08-27 13:36:23 -0400

    added upload_file via s3 sdk

--- drop_receptacle.rb
+++ drop_receptacle.rb
@@ -1,14 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
 require 'yaml'
-require 'net/http'
+require 'aws-sdk'
 
 def load_config
     config = YAML.load_file('config.yaml')
     @access_key_id = config['s3_info']['access_key_id']
     @secret_access_key = config['s3_info']['secret_access_key']
     @bucket = config['s3_info']['bucket']
-    puts @access_key_id
-    puts @secret_access_key
-    puts @bucket
+    AWS.config(
+      :access_key_id =>  @access_key_id, 
+      :secret_access_key => @secret_access_key
+    )
+    @s3 = AWS::S3.new
+end
+
+def upload_file(file_name)
+    key = File.basename(file_name)
+    @s3.buckets[@bucket].objects[key].write(:file => file_name)
+    puts "Uploading file #{file_name} to bucket #{@bucket}."
 end
 
 load_config
+
+upload_file('billstripe/readme.txt')
